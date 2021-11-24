ca21ee41345e9e0eca06727a6b8a47d6aa00c131
Bebebe <visio_O@mail.ru>
2021-11-25 00:14:27 +0300

    bubblesort longest before falling

--- bubblesort.asm
+++ bubblesort.asm
@@ -10,7 +10,6 @@ BubbleSortContainer2:
 section .data
     numFmt  db  "%d: ",0
     real dq 0.0
-    outfmt db "Complex number (%d, %d) - %g",10,0
 section .bss
     pcont  resq    1   ; адрес контейнера
     len    resd    1   ; адрес для сохранения числа введенных элементов
@@ -18,31 +17,38 @@ section .bss
     cntr   resd    1   ; вспомогательный счетчик
     cntr2   resd    1   ; вспомогательный счетчик
     lptr    resd    1   ; вспомогательный счетчик
-    firstad resq 1
-    first resq 1
-    second resq 1
-    .p      resq  1       ; вычисленный периметр прямоугольника
 
 section .text
 push rbp
 mov rbp, rsp
 
     mov [pcont], rdi   ; сохраняется указатель на контейнер
+    ; PrintInt2 rdi, [stdout]
+    
     mov [len],   esi     ; сохраняется число элементов
     mov [FILE],  rdx    ; сохраняется указатель на файл
 	mov cl, [len]					;set cl with array length
 	mov [cntr], cl					;set this variable with array length
 
     PrintStr2 "real0:", [FILE]
+    mov rdi, [pcont]
+    call ToRealNumber     ; Получение периметра первой фигуры
     
-    mov     rdi, [pcont]
-    add rdi, 4
-    mov     rsi, [FILE]
-    mov [firstad], rdi
-    call    OutComplexNumber
+    PrintStr2 "real:", [FILE]
+    movsd [real], xmm0
+    PrintDouble2 xmm0, [FILE]
+    PrintStr2 "rend:", [FILE]
 
+    mov rax, [pcont + 1]
+    PrintInt2 rax, [FILE]
+    PrintStr2 "first:", [FILE]
+    PrintInt2 [pcont + 1], [FILE]
+    PrintStr2 "second:", [FILE]
+    PrintInt2 [pcont + 2], [FILE]
+    PrintStr2 "entered:", [FILE]
+    
     up1 :
-	mov rdi, [pcont]						;Points to start
+	mov rdi, [pcont + 1]						;Points to start
 	mov al, [cntr]					;
 	mov bl, [lptr]
 	sub al, bl						; n-i (Rather to increase i, Decrements n)
@@ -52,38 +58,20 @@ mov rbp, rsp
     PrintContainer2 [pcont], [len], [FILE]
 
 	up2:
-        ; PrintStr2 "entered 3", [FILE]
-        ; PrintContainer2 [pcont], [len], [FILE]
-        ; PrintInt2 [pcont], [FILE]
+        PrintStr2 "entered 3", [FILE]
+        PrintContainer2 [pcont], [len], [FILE]
+        PrintInt2 [pcont], [FILE]
         ; PrintInt2 [pcont+1], [FILE]
 
-        add rdi, 4
-        mov [first], rdi
-		mov r8, [rdi]				;Get [j] in r8
-		add rdi, 16
-        mov r9, [rdi]			;Get [j+1]in bl
-		cmp r8, r9
-		jb skip		 				;Compares
-        PrintStr2 "didnt enter skip", [FILE]
-
-        sub rdi, 20
-        mov r8, [rdi + 16]					;If larger swapped
-		mov [rdi + 16], rdi
-        mov [rdi], r8
-        add rdi, 4
-        mov r8, [rdi + 16]					;If larger swapped
-		mov [rdi + 16], rdi
-        mov [rdi], r8
-        add rdi, 4
-        mov r8, [rdi + 16]					;If larger swapped
-		mov [rdi + 16], rdi
-        mov [rdi], r8
-        add rdi, 4
-
-        skip:
+		mov al, [rdi]				;Get [j] in al
+		mov bl, [rdi+1]			;Get [j+1]in bl
+		cmp al, bl
+		jb skip						;Compares
+		mov [rdi+1], al					;If larger swapped
+		mov [rdi], bl
+		skip:
             PrintStr2 "entered skip", [FILE]
-            ; sub rdi, 4
-		    add rdi, 16			;Else continues to next number
+		    add rdi, 1			;Else continues to next number
 		    sub byte[cntr2], 1
 		    jnz up2
 			
